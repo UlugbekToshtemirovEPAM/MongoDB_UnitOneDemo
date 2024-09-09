1ffeac23e0361e49c5c3a486a60ecd69f7539820
UlugbekToshtemirovEPAM <Ulugbek_Toshtemirov@epam.com>
2024-09-09 14:19:03 +0500

    Add project files.

---
+++ MongoDB_UnitOneDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+
+var mongoURL = new MongoUrl("mongodb+srv://myAtlasDBUser:MongoParol123@myatlasclusteredu.vqhvw.mongodb.net/");
+var client = new MongoClient(mongoURL);
+
+var dbList = client.ListDatabases().ToList();
+
+Console.WriteLine("The list of databases on this server is: ");
+foreach (var db in dbList)
+{
+    Console.WriteLine(db);
+}
